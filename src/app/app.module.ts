91e645dccd5993de7dfb8c272159a12175dfb56a
bpark <burt.parkers@googlemail.com>
2018-02-24 10:00:39 +0100

    added table module and winstrap image fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
 import {AlertModule, BsDatepickerModule} from 'ngx-bootstrap';
-import { OrderStepComponent } from './order-step/order-step.component';
-import { OrderScreenComponent } from './order-screen/order-screen.component';
-import { OrderPressDataComponent } from './order-press-data/order-press-data.component';
-import { OrderResultComponent } from './order-result/order-result.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { OrderGlueComponent } from './order-glue/order-glue.component';
-import { OrderDetailsComponent } from './order-details/order-details.component';
+import {OrderStepComponent} from './order-step/order-step.component';
+import {OrderScreenComponent} from './order-screen/order-screen.component';
+import {OrderPressDataComponent} from './order-press-data/order-press-data.component';
+import {OrderResultComponent} from './order-result/order-result.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {OrderGlueComponent} from './order-glue/order-glue.component';
+import {OrderDetailsComponent} from './order-details/order-details.component';
+import {RouterModule, Routes} from '@angular/router';
+import {OrderDetailsTableComponent} from './order-details-table/order-details-table.component';
 
+
+const appRoutes: Routes = [
+  {path: 'glue', component: OrderGlueComponent},
+  {path: 'pressdata', component: OrderPressDataComponent},
+  {path: 'details', component: OrderDetailsComponent},
+  {path: 'detailstbl', component: OrderDetailsTableComponent},
+  {path: '**', component: OrderPressDataComponent}
+];
 
 @NgModule({
   declarations: [
@@ -23,9 +33,14 @@ import { OrderDetailsComponent } from './order-details/order-details.component';
     OrderResultComponent,
     NavbarComponent,
     OrderGlueComponent,
-    OrderDetailsComponent
+    OrderDetailsComponent,
+    OrderDetailsTableComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      {enableTracing: true}
+    ),
     BrowserModule,
     FormsModule,
     AlertModule.forRoot(),
@@ -34,4 +49,5 @@ import { OrderDetailsComponent } from './order-details/order-details.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/order-details-table/order-details-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-order-details-table',
+  templateUrl: './order-details-table.component.html',
+  styleUrls: ['./order-details-table.component.css']
+})
+export class OrderDetailsTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
