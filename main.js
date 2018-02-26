52a736a3afdb39b8f9893633c2874465dac0d71f
bpark <burt.parkers@googlemail.com>
2018-02-26 13:22:30 +0100

    updated .gitignore and disabled node integration

--- main.js
+++ main.js
@@ -5,14 +5,17 @@ let win;
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({
-        width: 600,
-        height: 600,
+        width: 1200,
+        height: 900,
         backgroundColor: '#ffffff',
+        titleBarStyle: 'hidden',
+        webPreferences: {
+          nodeIntegration: false
+        },
         icon: `file://${__dirname}/dist/assets/logo.png`
-    })
+    });
 
-
-    win.loadURL(`file://${__dirname}/dist/index.html`)
+    win.loadURL(`file://${__dirname}/dist/index.html`);
 
     //// uncomment below to open the DevTools.
     // win.webContents.openDevTools()
@@ -24,7 +27,7 @@ function createWindow () {
 }
 
 // Create window on electron intialization
-app.on('ready', createWindow)
+app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
@@ -33,11 +36,11 @@ app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
-})
+});
 
 app.on('activate', function () {
     // macOS specific close process
     if (win === null) {
         createWindow()
     }
-})
+});
