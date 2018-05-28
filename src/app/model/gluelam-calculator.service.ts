4373c72eeed78e9abfe70e44da4a861c06e236aa
bpark <burt.parkers@googlemail.com>
2018-05-28 20:16:01 +0200

    fixed calculation errors

--- src/app/model/gluelam-calculator.service.ts
+++ src/app/model/gluelam-calculator.service.ts
@@ -22,19 +22,19 @@ export class GluelamCalculatorService {
     const singleLamella = Math.round(inputDetailHeight * 10 / glulamModel.pressData.laminationStrength);
     //  10x10 / 33.6 = 2,97
 
-    const totalLength = inputDetailLength + glulamModel.gluelamData.additionalLength; // 15?? + 10 cm
+    const totalLength = inputDetailLength * 100 + glulamModel.gluelamData.additionalLength; // 15?? + 10 cm
     console.log('inputDetailLength: ' + inputDetailLength);
     console.log('addLen: ' + glulamModel.gluelamData.additionalLength);
     console.log('totalLength: ' + totalLength);
-    const totalWidht = glulamModel.gluelamData.width; // 8.5
-    const singleSquare = totalLength * totalWidht * (singleLamella - 1); // 25 * 8.5 * (3-1)
+    const totalWidht = glulamModel.gluelamData.width + 1; // 9.5
+    const singleSquare = totalLength * totalWidht * (singleLamella - 1); // 25 * 9.5 * (3-1)
     console.log('single square: ' + singleSquare);
 
     const outputDetailLamella = singleLamella * inputDetailAmount;
-    const outputDetailSquare = singleSquare * inputDetailAmount;
-    const outputDetailTotalSquare = outputDetailLamella + singleSquare;
+    const outputDetailSquare = singleSquare / 10000;
+    const outputDetailTotalSquare = inputDetailAmount * singleSquare / 10000;
 
-    const outputRowVolume = glulamModel.gluelamData.width * inputDetailHeight * inputDetailLength * inputDetailAmount;
+    const outputRowVolume = glulamModel.gluelamData.width * inputDetailHeight * inputDetailLength * inputDetailAmount / 10000;
 
     this.calculationResult.lamination -= outputDetailLamella;  // TODO: error if negative
 
