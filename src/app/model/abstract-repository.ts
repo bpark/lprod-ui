60f549fc4a87f17dfd73c85cff0b0a4f5b1b7fbc
bpark <burt.parkers@googlemail.com>
2018-08-22 13:53:39 +0200

    added JSDoc

--- src/app/model/abstract-repository.ts
+++ src/app/model/abstract-repository.ts
@@ -9,9 +9,16 @@ export class AbstractRepository<T extends BaseEntity> {
   private static readonly page = 'page';
   private static readonly pageSize = 'pageSize';
 
+
   constructor(protected http: HttpClient, protected jwtTokenStore: JwtTokenStoreService, private apiUrl: string) {
   }
 
+  /**
+   * Returns a list of entities. The returned list is a subclass of BaseEntityList.
+   * @param page the page to retrieve.
+   * @param pageSize the pageSize of the page.
+   * @param queryParams optional queryParameters added to the request.
+   */
   list<S extends BaseEntityList<T>>(page: number, pageSize: number, queryParams?: HttpParams): Observable<S> {
     let httpParams = new HttpParams()
       .set(AbstractRepository.page, String(page))
@@ -29,24 +36,40 @@ export class AbstractRepository<T extends BaseEntity> {
     });
   }
 
+  /**
+   * Returns an entity by it's id.
+   * @param id the entities id.
+   */
   get(id: number): Observable<T> {
     return this.http.get<T>(this.createConnectionUrl(id), {
       headers: this.jwtTokenStore.createTokenHeader()
     });
   }
 
+  /**
+   * Persists a new entity.
+   * @param entity the entity to persist.
+   */
   create(entity: T): Observable<number> {
     return this.http.post<number>(this.createConnectionUrl(), entity, {
       headers: this.jwtTokenStore.createTokenHeader()
     });
   }
 
+  /**
+   * Updates an existing entity. The id of the entity must set.
+   * @param entity the entity to update.
+   */
   update(entity: T): Observable<void> {
     return this.http.put<void>(this.createConnectionUrl(entity.id), entity, {
       headers: this.jwtTokenStore.createTokenHeader()
     });
   }
 
+  /**
+   * Deletes an existing entity.
+   * @param id the id of the entity to delete.
+   */
   delete(id: number): Observable<void> {
     return this.http.delete<void>(this.createConnectionUrl(id), {
       headers: this.jwtTokenStore.createTokenHeader()
