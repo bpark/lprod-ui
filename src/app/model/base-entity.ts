d257fd3efecbee3d1fad8b41f4d80135a6d11122
bpark <burt.parkers@googlemail.com>
2018-08-22 13:59:45 +0200

    added JSDoc

--- src/app/model/base-entity.ts
+++ src/app/model/base-entity.ts
@@ -1,8 +1,21 @@
+/**
+ * Interface for all entity lists.
+ *
+ * A entity list contains the total count of available entities and a subset of retrieved entities defined by paging parameters.
+ */
 export interface BaseEntityList<T> {
+  /** total count of available entities. */
   totalCount: number;
+  /** amount of entities in this list, defined by paging parameters. */
   items: T[];
 }
 
+/**
+ * Base class for all entities.
+ *
+ * The base class defines a set of common entity attributes shared by all entities.
+ */
 export abstract class BaseEntity {
+  /** Id of the entity. */
   id: number;
 }
