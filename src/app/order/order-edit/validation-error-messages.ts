3486d381bb26ea92dd52b890cdc6e1627a751aa1
bpark <burt.parkers@googlemail.com>
2018-08-28 15:12:57 +0200

    added validation for all main fields

--- src/app/order/order-edit/validation-error-messages.ts
+++ src/app/order/order-edit/validation-error-messages.ts
@@ -5,6 +5,23 @@ export class ValidationErrorMessages {
     min: 'Der Wert muss gößer 10 sein'
   };
 
+  private static additionalLength = {
+    required: 'Die Eingabe darf nicht leer sein',
+    min: 'Der Wert muss gößer 5 sein'
+  };
+
+  private static hardenerPercentage = {
+    required: 'Die Eingabe darf nicht leer sein',
+    min: 'Der Wert muss gößer oder gleich 0 sein',
+    max: 'Der Wert darf nicht größer als 100 sein'
+  };
+
+  private static laminationStrength = {
+    required: 'Die Eingabe darf nicht leer sein',
+    min: 'Der Wert muss gößer oder gleich 0 sein',
+    max: 'Der Wert darf nicht größer als 100 sein'
+  };
+
   static getErrorText(id: string, error: string) {
     const definition = this[id];
     console.log('definition: ', definition);
