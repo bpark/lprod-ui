78b60f57b4c79cda92a7a540c6192a751525efda
bpark <burt.parkers@gmail.com>
2018-09-17 15:31:14 +0200

    fixed compile error

--- src/app/components/digit.directive.spec.ts
+++ src/app/components/digit.directive.spec.ts
@@ -2,7 +2,7 @@ import { DigitDirective } from './digit.directive';
 
 describe('DigitDirective', () => {
   it('should create an instance', () => {
-    const directive = new DigitDirective();
+    const directive = new DigitDirective(null);
     expect(directive).toBeTruthy();
   });
 });
