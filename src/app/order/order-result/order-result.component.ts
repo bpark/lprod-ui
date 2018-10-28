4879eb8ff28132e2a9fbbf7533aedd2a11aebcd5
bpark <burt.parkers@googlemail.com>
2018-10-28 09:11:26 +0100

    fixed compile errors

--- src/app/order/order-result/order-result.component.ts
+++ src/app/order/order-result/order-result.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {GlulamOrderService} from '../../model/glulam-order.service';
-import {CalculationResult, GlulamModel} from '../../model/glulam.model';
-import {GluelamCalculatorService} from '../../model/gluelam-calculator.service';
 
 @Component({
   selector: 'app-order-result',
@@ -10,11 +7,7 @@ import {GluelamCalculatorService} from '../../model/gluelam-calculator.service';
 })
 export class OrderResultComponent implements OnInit {
 
-  glulamModel: GlulamModel;
-  calculationResult: CalculationResult;
-
-  constructor(private glulamOrderService: GlulamOrderService,
-              private gluelamCalculatorService: GluelamCalculatorService) { }
+  constructor() { }
 
   ngOnInit() {
     // this.glulamModel = this.glulamOrderService.glulamModel;
